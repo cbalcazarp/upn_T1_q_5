dd29ae11d59bcb374d1b6d70590001562461e2ed
Carlos Balcazar <cbalcazarp@gmail.com>
2021-08-28 15:18:03 -0500

    removed variable

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace upn_T1_q_5
             Console.WriteLine("Conversión de grados Kelvin a grados Fahrenheit y Celsius");
             Console.Write("Ingrese grados Kelvin: ");
             gradosKelvin = Convert.ToDouble(Console.ReadLine());
-            gradosFahrenheit = (32*gradosKelvin - 273.15) * 9/5 + 32;
-            gradosCelsius = 32*gradosKelvin − 273.15;
+            gradosFahrenheit = (gradosKelvin - 273.15) * 9/5 + 32;
+            gradosCelsius = gradosKelvin - 273.15;
             Console.WriteLine("Lo ingresado en grados Fahrenheit es " + Convert.ToString(gradosFahrenheit) + " y en grados Celsius es " + Convert.ToString(gradosCelsius));
 
 
