2babd4e5108ebfc700ef6db2712738093c7bac62
Carlos Balcazar <cbalcazarp@gmail.com>
2021-08-28 15:15:07 -0500

    code added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace upn_T1_q_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Double gradosKelvin, gradosFahrenheit, gradosCelsius;
+            Console.WriteLine("Conversión de grados Kelvin a grados Fahrenheit y Celsius");
+            Console.Write("Ingrese grados Kelvin: ");
+            gradosKelvin = Convert.ToDouble(Console.ReadLine());
+            gradosFahrenheit = (32*gradosKelvin - 273.15) * 9/5 + 32;
+            gradosCelsius = 32*gradosKelvin − 273.15;
+            Console.WriteLine("Lo ingresado en grados Fahrenheit es " + Convert.ToString(gradosFahrenheit) + " y en grados Celsius es " + Convert.ToString(gradosCelsius));
+
+
         }
     }
 }
